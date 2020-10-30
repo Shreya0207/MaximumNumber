4216240f77356a9613ee18f4b865442b1802936a
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-30 19:48:29 +0530

    [Shreya]Add: Evaluating the maximum of three integers

---
+++ MaximumNumberProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MaximumNumberProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Maximum Number Program using Generics");
+            Console.WriteLine("Find max Number");
+            MaximumNumberCheck MaxInt = new MaximumNumberCheck();
+            int output = MaxInt.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
